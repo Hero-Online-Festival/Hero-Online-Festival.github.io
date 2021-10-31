8f6378c5a7fffa5ab0ac93bb8ad5a9c79744c653
박용준 <9898junjun2@gmail.com>
2021-10-31 21:26:47 +0900

    og tag

---
+++ src/common/HeroGame.ts
@@ -0,0 +1,13 @@
+type TName = {
+  phone: string;
+  stuNum: string;
+  num: number;
+};
+
+export const HeroGame: Record<string, TName> = {
+  ["박용준"]: {
+    phone: "01040597883",
+    stuNum: "201711147",
+    num: 1,
+  },
+};
